05d66e95e9588e219a5b9a714218b7758e39e831
Dmytro <dima.sichkar2003@gmail.com>
2024-12-09 15:14:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,44 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self, name:
+            str, health: int = 100,
+            hidden: bool = False
+    ) -> None:
+
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        if self.health > 0:
+            Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{"
+                f"Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}"
+                f"}}")
+
+    @classmethod
+    def update_alive(cls) -> None:
+        cls.alive = [animal for animal in cls.alive if animal.health > 0]
+
+    @classmethod
+    def __str__(cls) -> str:
+        return f"[{", ".join(repr(animal) for animal in cls.alive)}]"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(beast: Animal) -> None:
+        if isinstance(beast, Herbivore) and not beast.hidden:
+            beast.health -= 50
+            if beast.health <= 0:
+                beast.health = 0
+                Animal.update_alive()
